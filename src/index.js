d30fdc66a3704c17cee0125ed5a276f829a529a5
Francesco deCollibus <francesco.decollibus@business.uzh.ch>
2021-11-09 15:25:05 +0100

    Added ethers connection

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom';
 import { AXADropdown } from './patterns-library';
 import { AXAButton } from './patterns-library';
 import { AXAHeroBanner } from './patterns-library';
-
+import { AXAHeading } from './patterns-library';
+import { useState } from 'react';
+import { ethers } from 'ethers'
 
 import './index.scss';
 
@@ -26,10 +28,57 @@ export default class PodCryptoEasyInvest {
   }
 
   init() {
+    //const provider = new ethers.providers.Web3Provider(window.ethereum)
+    requestAccount()
     ReactDOM.render(<>
-      <AXAHeroBanner></AXAHeroBanner>
+      <AXAHeroBanner imageSource='img/millionaire.webp'>
+        Become hilariously rich with Crypto!
+
+      </AXAHeroBanner>
+      <AXAHeading rank="1">Become hilariously rich with Crypto!</AXAHeading>
       <AXADropdown items={[{ name: 'Ether', value: 'Ether 1' }]}></AXADropdown>
       <AXAButton icon="arrow-right">Crypto Easy Invest</AXAButton>
     </>, this.elem);
   }
+  
+
+
 }
+
+
+
+//import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
+
+  // request access to the user's MetaMask account
+  async function requestAccount() {
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  }
+  const greeterAddress='0x0087EB397af9E04Ff9872199d63F841474bf2A27'
+  // call the smart contract, read the current greeting value
+  async function fetchGreeting() {
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+      try {
+        const data = await contract.greet()
+        console.log('data: ', data)
+      } catch (err) {
+        console.log("Error: ", err)
+      }
+    }    
+  }
+
+  
+/*
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={fetchGreeting}>Fetch Greeting</button>
+        <button onClick={setGreeting}>Set Greeting</button>
+        <input onChange={e => setGreetingValue(e.target.value)} placeholder="Set greeting" />
+      </header>
+    </div>
+  );
+}
+
+export default App;*/
--- src/patterns-library.js
+++ src/patterns-library.js
@@ -3,7 +3,9 @@ import { createElement } from 'react';
 import createAXAButtonReact from '@axa-ch/button/lib/index.react';
 import createAXADropdown from '@axa-ch/dropdown/lib/index.react';
 import createAXACommercialHeroBanner from '@axa-ch/commercial-hero-banner/lib/index.react'
+import  createAXAHeading from '@axa-ch/heading/lib/index.react'
 
 export const AXAButton = createAXAButtonReact(createElement);
 export const AXADropdown = createAXADropdown(createElement);
 export const AXAHeroBanner = createAXACommercialHeroBanner(createElement);
+export const AXAHeading = createAXAHeading(createElement);
